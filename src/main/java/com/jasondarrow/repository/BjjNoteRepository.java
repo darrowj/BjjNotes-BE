37dbbb6bac87d96217ab7593f41f736062bc7edb
Jason Darrow <darrowj@gmail.com>
2017-07-29 11:38:59 -0400

    CRUD tweaks

---
+++ src/main/java/com/jasondarrow/repository/BjjNoteRepository.java
@@ -0,0 +1,21 @@
+package com.jasondarrow.repository;
+
+import com.jasondarrow.model.Note;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Repository;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * Created by darrowj on 7/19/17.
+ */
+@Repository
+public class BjjNoteRepository   {
+
+
+
+}
