aca7e606d21569b8d33b28b460f78ef9daa0c33e
Jason Darrow <darrowj@gmail.com>
2017-11-16 05:51:28 -0500

    Removed Seeder to avoid confusion in data

--- src/main/java/com/jasondarrow/repository/DbSeeder.java
+++ src/main/java/com/jasondarrow/repository/DbSeeder.java
@@ -21,19 +21,19 @@ public class DbSeeder implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         List<Note> notes = new ArrayList<Note>();
-        Note one = new Note("5th", "THis is my dsfgt note");
-        Note two = new Note("3rd Note", "This is my sdfg note");
-        Note three = new Note("4th Note", "This is my dsfguyf note");
-        Note four = new Note("6th", "THis is my dsfgt note");
-        Note five = new Note("7rd Note", "This is my sdfg note");
-        Note six = new Note("8th Note", "This is my dsfguyf note");
+        //Note one = new Note("5th", "THis is my dsfgt note");
+        //Note two = new Note("3rd Note", "This is my sdfg note");
+        //Note three = new Note("4th Note", "This is my dsfguyf note");
+        //Note four = new Note("6th", "THis is my dsfgt note");
+        //Note five = new Note("7rd Note", "This is my sdfg note");
+        //Note six = new Note("8th Note", "This is my dsfguyf note");
         //this.noteRepository.deleteAll();
 
-        notes.add(one);
-        notes.add(two);
-        notes.add(three);
+        //notes.add(one);
+        //notes.add(two);
+        //notes.add(three);
 
-        this.noteRepository.save(notes);
+        //this.noteRepository.save(notes);
     }
 
 
